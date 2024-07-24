fd7cd84e1cc3f0af5e0a3fede117fff830a12347
OmarKHDR <omarsliman37@gmail.com>
2024-07-24 21:49:35 +0300

    adding 100-textme.rb file

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # rrrr
 
-puts ARGV[0].scan(/(?<=from:)[1-9A-Za-z]*(?<=from:)[1-9A-Za-z]*(?<=flags:)[1-9:-]*/).join(',')
+puts ARGV[0].scan(/(?<=from:)[1-9A-Za-z]*|(?<=from:)[1-9A-Za-z]*|(?<=flags:)[1-9:-]*/).join(',')
