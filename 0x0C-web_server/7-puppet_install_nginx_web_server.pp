0c86fc57f9e2bf13c3073af867ac32b3ffcb5c2d
OmarKHDR <omarsliman37@gmail.com>
2024-08-14 15:52:51 +0300

    ..

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,24 @@
+## I just want to finish this task
+package { 'nginx':
+    ensure => 'present'
+}
+
+exec { 'installing':
+    command => 'sudo apt-get -y update; sudo apt-get -y install nginx',
+    provider => shell
+}
+
+exec { 'file':
+    command => 'echo "Hello World!" | sudo tee /var/www/html/index.html',
+    provider => shell
+}
+
+exec { 'redirecting':
+    command => 'sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:www.facebook.com permanent;/" /etc/nginx/sites-available/default',
+    provider => shell
+}
+
+exec { 'finishing':
+    command => 'sudo service nginx restart',
+    provider => shell
+}
