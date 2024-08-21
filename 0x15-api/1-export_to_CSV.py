c4226619009482eaf7f5f0dc7b86009181532a2c
OmarKHDR <omarsliman37@gmail.com>
2024-08-21 19:13:58 +0300

    defw

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""docs are docs"""
+import requests
+import sys
+
+
+#"USER_ID","USERNAME","TASK_COMPLETED_STATUS","TASK_TITLE"
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    empId = sys.argv[1]
+    res1 = requests.get(url+"users"+f"?id={empId}")
+    empName = res1.json()[0]['username']
+
+    res2 = requests.get(url+"todos"+f"?userId={empId}")
+    res2 = res2.json()
+    with open('USER_ID.csv', 'w') as f:    
+        for task in res2:
+            line = f"\"{empId}\",\"{empName}\","
+            line += f"\"{task['completed']}\",\"{task['title']}\"\n"
+            f.write(line)
