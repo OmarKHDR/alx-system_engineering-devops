d11550e1ba0894c5bc3d3b204b21b73150a1b96d
OmarKHDR <omarsliman37@gmail.com>
2024-08-21 20:01:21 +0300

    1.0

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""docs are docs"""
+import requests
+import json
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
+    empId = sys.argv[1]
+    res1 = requests.get(url+"users"+f"?id={empId}")
+    empName = res1.json()[0]['username']
+
+    res2 = requests.get(url+"todos"+f"?userId={empId}")
+    res2 = res2.json()
+    user_arr = []
+    for task in res2:
+        task['username'] = empName
+        user_arr.append(task)
+
+    obj = {f'\"{empId}\"': user_arr}
+    json_obj = json.dumps(obj)
+    with open(f"{empId}.json", 'w') as f:
+        f.write(json_obj)
