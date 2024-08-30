d300a717cce250fb3e92fdbda89b981116cf47ca
OmarKHDR <omarsliman37@gmail.com>
2024-08-30 16:16:54 +0300

    pls

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,30 +1,20 @@
 #!/usr/bin/python3
-"""First script that uses Reddit API"""
+""" 1docs docs """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """A function to do stuff that can be done using other functions"""
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-    user_agent = "frog-in-well"
-    headers = {"User-Agent": user_agent}
+    """ pls accept """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    res = requests.get(url, headers=headers, allow_redirects=True)
-    try:
-        dic = res.json()
-    except:
-        return 0
-    if res.status_code >= 400:
+    headers = {
+        'User-Agent': 'frog-in-well'
+    }
+    res = requests.get(url, headers)
+    if res.status_code != 200:
         return 0
     else:
-        return dic['data']['subscribers']
-
-import sys
-
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+        try:
+            return res.json()['data']['subscribers']
+        except:
+            pass
+    return 0
