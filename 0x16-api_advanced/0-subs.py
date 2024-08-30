f49aa3039103079c8ee5150dee5057d053ee4bbf
OmarKHDR <omarsliman37@gmail.com>
2024-08-30 12:04:14 +0300

    pls

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
-"""First script that uses Reddit API"""
 import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+    my func
+    """
     """A function to do stuff that can be done using other functions"""
     if subreddit is None or type(subreddit) is not str:
         return 0
-    user_agent = "0x16-api_advanced/1.0 by AMR856"
+    user_agent = "0x16-api_advanced/1.0 by frog-in-well"
     headers = {"User-Agent": user_agent}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     my_object = requests.get(url, headers=headers, allow_redirects=False)
@@ -16,3 +18,4 @@ def number_of_subscribers(subreddit):
         return my_dict['data']['subscribers']
     else:
         return 0
+
