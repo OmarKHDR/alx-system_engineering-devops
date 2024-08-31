463420ffdeeff75b4e115160a77832997aeaa17e
OmarKHDR <omarsliman37@gmail.com>
2024-08-31 18:19:55 +0300

    wow

--- 0x16-api_advanced/parseReddit.py
+++ 0x16-api_advanced/parseReddit.py
@@ -3,6 +3,32 @@ import requests
 import sys
 
 
+def print_comments(comment_list):
+    if comment_list == []:
+        print("no replies")
+        return
+    else:
+        for child in comment_list:
+            print(child['data']['body'])
+            if child['data']['replies'] == "":
+                continue
+            print_comments(child['data']['replies']['data']['selftext'])
+
+
+def print_content_and_comments(post, headers):
+    url = f"https://www.reddit.com{post['data']['permalink']}.json"
+    body = requests.get(url, headers=headers)
+    try:
+        body = body.json()
+        if body[0]['data']['children'][0]['data']['selftext'] == "":
+            print("no text content, cant desplay media")
+        else:
+            print(body[0]['data']['children'][0]['data']['selftext'])
+        print_comments(body[1]['data']['children'])
+    except Exception as e:
+        print("error in post ", e)
+
+
 def search(keyword):
     after = 'null'
     while (after is not None):
@@ -18,13 +44,9 @@ def search(keyword):
             res = res.json()
             for post in res['data']['children']:
                 print(post['data']['title'],":")
-                if(post['data']['selftext'] == ""):
-                    print("no content")
-                else:
-                    print(post['data']['selftext'])
-                print("\n")
-        except:
-            print("problem with result")
+                print_content_and_comments(post, headers)
+        except Exception as e:
+            print("problem with result", e)
         after = res['data']['after']
 
 if __name__ == '__main__':
